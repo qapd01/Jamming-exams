a35f84adbcaf73f3e637d1ed224ba76c2906e4b0
Sorawid Mingmolee <bossxlp@gmail.com>
2022-07-20 15:18:06 +0700

    w8 for sopt token

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import Track from "../Track/Track";
 
-//import TrackList.css
+import './TrackList.css'
+
+
 
 class TrackList extends Component {
   render() {
-    return <div>TrackList</div>;
+    return (
+      <div className="TrackList">
+        {
+          this.props.tracks.map(track => {
+            return <Track track={track}
+                            key={track.id}
+                            onAdd={this.props.onAdd}
+                            onRemove={this.props.onRemove}
+                            isRemoval={this.props.isRemoval}/>
+          })
+        }
+      </div>
+    )
   }
 }
 
